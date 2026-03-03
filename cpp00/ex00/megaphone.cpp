1c549fb260042c73137ef5b1457e8a5e49b01ef0
hieutrpham <hieutrungpham91@gmail.com>
2026-03-03 10:18:21 +0200

    feat: megaphone

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,6 +1,19 @@
+#include <cctype>
 #include <iostream>
+#include <string>
 
-int main() {
-	std::cout << "Hellow world";
+int main(int ac, char **av) {
+	if (ac == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	std::locale loc;
+	for (int i = 1; i < ac; i++) {
+		std::string str = av[i];
+		for (int i = 0; i < str.length(); i++) {
+			std::cout << std::toupper(str[i], loc);
+		}
+	}
+	std:: cout << std::endl;
 	return 0;
 }
