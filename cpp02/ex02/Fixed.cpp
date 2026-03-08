f24b5df8755fecef6ff649a64fb80ebed8a01ed5
Hieu Pham <hieutrungpham91@gmail.com>
2026-03-08 16:25:02 +0200

    cpp02 ex02

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,141 @@
+#include "Fixed.hpp"
+#include <cmath>
+#include <iostream>
+#include <ostream>
+
+Fixed::Fixed() : num(0) {
+	// std::cout << "default constructor called\n";
+}
+
+Fixed::Fixed(int param) : num(param << bit) {
+	// std::cout << "int constructor called\n";
+}
+
+Fixed::Fixed(float param) : num(std::roundf(param * (1 << bit))) {
+	// std::cout << "float constructor called\n";
+}
+
+Fixed::~Fixed() {
+	// std::cout << "destructor called\n";
+}
+
+Fixed::Fixed(const Fixed &copy) : num(copy.num){
+	// std::cout << "copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	// std::cout << "copy assignment operator called\n";
+	if (this != &other)
+		this->num = other.num;
+	return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	// std::cout << "getRawbits member function called\n";
+	return this->num;
+}
+
+void Fixed::setRawBits(int const raw) {
+	// std::cout << "setRawbits member function called\n";
+	this->num = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return (float)this->num / (1 << this->bit);
+}
+
+int Fixed::toInt(void) const {
+	return this->num >> this->bit;
+}
+
+bool Fixed::operator!=(const Fixed& other) {
+	return this->num != other.num;
+}
+bool Fixed::operator==(const Fixed& other) {
+	return this->num == other.num;
+}
+
+bool Fixed::operator>=(const Fixed& other) {
+	return this->num >= other.num;
+}
+
+bool Fixed::operator<=(const Fixed& other) {
+	return this->num <= other.num;
+}
+bool Fixed::operator>(const Fixed& other) {
+	return this->num > other.num;
+}
+
+bool Fixed::operator<(const Fixed& other) {
+	return this->num < other.num;
+}
+
+Fixed Fixed::operator+(const Fixed& other) {
+	return Fixed(this->toFloat() + other.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed& other) {
+	return Fixed(this->toFloat() - other.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed& other) {
+	return Fixed(this->toFloat() * other.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed& other) {
+	return Fixed(this->toFloat() / other.toFloat());
+}
+
+Fixed& Fixed::operator--() {
+	this->num--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int) {
+	Fixed temp = *this;
+	operator--();
+	return temp;
+}
+Fixed& Fixed::operator++() {
+	this->num++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int) {
+	Fixed temp = *this;
+	operator++();
+	return temp;
+}
+
+const Fixed& Fixed::min(Fixed& one, Fixed& two) {
+	if (one <= two)
+		return one;
+	else
+		return two;
+}
+
+const Fixed& Fixed::min(const Fixed& one, const Fixed& two) {
+	if (one.num <= two.num)
+		return one;
+	else
+		return two;
+}
+
+const Fixed& Fixed::max(Fixed& one, Fixed& two) {
+	if (one <= two)
+		return two;
+	else
+		return one;
+}
+
+const Fixed& Fixed::max(const Fixed& one, const Fixed& two) {
+	if (one.num <= two.num)
+		return two;
+	else
+		return one;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f) {
+	os << f.toFloat();
+	return os;
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <ostream>
+
+class Fixed {
+private:
+	int num;
+	static const int bit = 8;
+public:
+	Fixed();
+	Fixed(const int param);
+	Fixed(const float param);
+	~Fixed();
+	Fixed(const Fixed &copy);
+	Fixed& operator=(const Fixed& other);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+	bool operator>(const Fixed& other);
+	bool operator<(const Fixed& other);
+	bool operator>=(const Fixed& other);
+	bool operator<=(const Fixed& other);
+	bool operator==(const Fixed& other);
+	bool operator!=(const Fixed& other);
+	Fixed operator+(const Fixed& other);
+	Fixed operator-(const Fixed& other);
+	Fixed operator*(const Fixed& other);
+	Fixed operator/(const Fixed& other);
+	Fixed& operator--();
+	Fixed operator--(int);
+	Fixed& operator++();
+	Fixed operator++(int);
+	static const Fixed& min(Fixed& one, Fixed& two);
+	static const Fixed& min(const Fixed& one, const Fixed& two);
+	static const Fixed& max(Fixed& one, Fixed& two);
+	static const Fixed& max(const Fixed& one, const Fixed& two);
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& f);
