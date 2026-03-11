1beeb6f4845194c7d17157a53e0181524568010c
Hieu Pham <hieutrungpham91@gmail.com>
2026-03-11 07:52:21 +0200

    c04 e02: add pure virtual function to Animal

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -10,6 +10,6 @@ public:
 	virtual ~Animal();
 	Animal& operator=(Animal& other);
 	Animal(Animal& other);
-	virtual void makeSound() const;
+	virtual void makeSound() const = 0;
 	std::string getType() const;
 };
