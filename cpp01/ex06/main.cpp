185db83f608a08dfc13a917228aa97455a03abb2
Hieu Pham <hieutrungpham91@gmail.com>
2026-03-17 18:31:49 +0200

    feat: cpp01/ex06

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -8,11 +8,24 @@ int main(int ac, char **av) {
 		return 0;
 	}
 	std::string str = av[1];
-	std::cout << str;
 	std::string names[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
-	int level;
+	int level = -1;
+	for (int i = 0; i < 4; ++i) {
+		if (names[i] == str) {
+			level = i;
+			break;
+		}
+	}
+	if (level < 0) {
+		std::cout << "Nah\n";
+		return 0;
+	}
 	Harl h;
 	switch (level) {
-		case 0: h.complain(names[level]);
+		case 0: h.complain("DEBUG"); [[fallthrough]];
+		case 1: h.complain("INFO"); [[fallthrough]];
+		case 2: h.complain("WARNING"); [[fallthrough]];
+		case 3: h.complain("ERROR"); break;
+		default: std::cout << "Unknown\n";
 	}
 }
