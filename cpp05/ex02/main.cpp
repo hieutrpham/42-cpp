23c44b3d88b56add068b90200551905c334b7ec0
Hieu Pham <hieutrungpham91@gmail.com>
2026-05-06 09:14:17 +0300

    feat: add robotomy form

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -1,21 +1,23 @@
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include <iostream>
 #include <ostream>
 
 int main() {
 	try {
-		Bureaucrat b1("heiru", 150);
-		ShrubberyCreationForm a("a"), b("home"), c;
-		LOG(a);
-		LOG(b);
-		LOG(c);
-		ShrubberyCreationForm d(b);
-		LOG(d);
-		c = a;
-		b1.signForm(a);
-		b1.executeForm(a);
+		Bureaucrat b1("heiru", 1);
+		// ShrubberyCreationForm a("a");
+		RobotomyRequestForm b("b");
+		// LOG(a);
+		// LOG(b);
+		// LOG(c);
+		// ShrubberyCreationForm d(b);
+		// LOG(d);
+		// c = a;
+		b1.signForm(b);
+		b1.executeForm(b);
 	} catch (std::exception& e) {
 		ERR(e.what());
 	}
