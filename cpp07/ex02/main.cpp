d0f4448eef8e8a967ff131131cc2fd436b3c1a27
hieutrpham <hieutrungpham91@gmail.com>
2026-05-13 14:19:46 +0300

    chore: format

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -5,27 +5,22 @@ int main()
 {
 	try
 	{
-		std::cout << "=== 1. Testing Default Constructor ===" << std::endl;
 		Array<int> emptyArray;
 		std::cout << "emptyArray size: " << emptyArray.size() << std::endl;
 
-		std::cout << "\n=== 2. Testing Parameterized Constructor ===" << std::endl;
 		Array<int> intArray(5);
 		std::cout << "intArray size: " << intArray.size() << std::endl;
-
 		std::cout << "intArray[0] initial value: " << intArray[0] << std::endl;
 
 		for (unsigned int i = 0; i < intArray.size(); i++)
 			intArray[i] = i * 10;
 
-		std::cout << "\n=== 3. Testing Copy Constructor (Deep Copy) ===" << std::endl;
 		Array<int> copyArray(intArray);
 
 		intArray[0] = 999;
 		std::cout << "intArray[0]: " << intArray[0] << " (Modified)" << std::endl;
 		std::cout << "copyArray[0]: " << copyArray[0] << " (Unaffected!)" << std::endl;
 
-		std::cout << "\n=== 4. Testing Assignment Operator (Deep Copy) ===" << std::endl;
 		Array<int> assignedArray;
 		assignedArray = copyArray;
 
@@ -33,18 +28,14 @@ int main()
 		std::cout << "copyArray[1]: " << copyArray[1] << " (Modified)" << std::endl;
 		std::cout << "assignedArray[1]: " << assignedArray[1] << " (Unaffected!)" << std::endl;
 
-		std::cout << "\n=== 5. Testing with std::string ===" << std::endl;
 		Array<std::string> stringArray(3);
 		stringArray[0] = "Hello";
 		stringArray[1] = "Templates";
 		stringArray[2] = "World";
 		for (unsigned int i = 0; i < stringArray.size(); i++)
-			std::cout << stringArray[i] << " ";
+			std::cout << stringArray[i];
 		std::cout << std::endl;
 
-		std::cout << "\n=== 6. Testing Exeption Handling ===" << std::endl;
-
-		std::cout << "Attempting to access out of bounds index..." << std::endl;
 		std::cout << intArray[10] << std::endl; // should throw
 	}
 	catch (const std::exception& e) {
