d9ead727396b3b8644f562e8136c229123641954
Hieu Pham <hieutrungpham91@gmail.com>
2026-07-14 22:32:08 +0300

    fix(c09/ex00): parse date using std::chrono instead

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <chrono>
 
 class BitcoinExchange {
 private:
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -10,11 +10,6 @@ int main(int ac, char** av)
 	}
 
 	const std::string input = av[1];
-	if (input != "input.txt")
-	{
-		std::cerr << "Invalid input file\n";
-		return EXIT_FAILURE;
-	}
 
 	std::ifstream file("data.csv");
 	if (!file.is_open())
@@ -48,37 +43,56 @@ int main(int ac, char** av)
 		std::getline(iss, date, '|');
 		str_trim(date);
 
-		struct tm tm;
-		if (!strptime(date.c_str(), "%Y-%m-%d", &tm))
+		std::stringstream date_stream(date);
+		std::chrono::year_month_day ymd;
+		
+		date_stream >> std::chrono::parse("%F", ymd);
+
+		if (date_stream.fail() || date_stream.eof() || !ymd.ok())
 		{
-			std::cout << "Error: bad input => " << date << "\n";
+			std::cerr << "Error: bad input => " << date << "\n";
 			continue;
 		}
 
+		if (ymd < std::chrono::year_month_day{std::chrono::year{2009}, std::chrono::January, std::chrono::day{2}}) {
+			std::cerr << "Error: bad input => " << date << "\n";
+			continue;
+		}
+
+		auto data = coinbase.get_data();
+		auto price = data.lower_bound(date);
+
 		iss >> value;
+
+		if (ymd > std::chrono::year_month_day{std::chrono::year{2022}, std::chrono::March, std::chrono::day{29}}) {
+			price = data.lower_bound("2022-03-29");
+			std::cout << date << " => " << value << " = " << value * price->second << "\n";
+			continue;
+		}
+
 		if (value < 0)
 		{
-			std::cout << "Error: not a positive number.\n";
+			std::cerr << "Error: not a positive number.\n";
 			continue;
 		}
 
 		if (value > 1000)
 		{
-			std::cout << "Error: too large a number.\n";
+			std::cerr << "Error: too large a number.\n";
 			continue;
 		}
-		auto data = coinbase.get_data();
-		auto price = data.lower_bound(date);
+
+		// NOTE: give the last price point instead of error
 		if (price == data.end())
 		{
-			std::cout << "Error: bad input => " << date << "\n";
+			std::cerr << "Error: bad input => " << date << "\n";
 			continue;
 		}
 
 		if (iss.eof())
 			std::cout << date << " => " << value << " = " << value * price->second << "\n";
 		else
-			std::cout << "Error: bad format\n";
+			std::cerr << "Error: bad format\n";
 	}
 }
 
